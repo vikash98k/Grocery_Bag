027a9f96cc8117f0e8d6eb938321f612afc5e2e0
vikash999 <vikashkarodiya0000@gmail.com>
2021-03-12 18:32:08 +0530

    final commit

--- users/models.py
+++ users/models.py
@@ -14,7 +14,7 @@ class Profile(models.Model):
 
 		img = Image.open(self.image.path)
 
-		if(img.height>300 or img.width>300):
-			output_size =(300,300)
+		if(img.height>200 or img.width>200):
+			output_size =(200,200)
 			img.thumbnail(output_size)
 			img.save(self.image.path)
